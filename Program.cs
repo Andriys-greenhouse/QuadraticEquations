2be46eee1b601fd0e860e3ecb45423cdd2ddde54
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-06-06 20:23:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquations
+{
+    public class EquationsSolver
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public QuadraticResult QuadraticEquationSolver(decimal a, decimal b, decimal c)
+        {
+
+        }
+    }
+
+    public class QuadraticResult
+    {
+        public bool Solved { get; set; }
+        public decimal[] Results { get; set; }
+
+        public QuadraticResult(bool aSolved, decimal[] aResults)
+        {
+            Solved = aSolved;
+            Results = aResults;
+        }
+    }
+}
